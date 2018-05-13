815c056fb9ba2c6dca36da9f73cf58db3b7a2348
Matt Perry <mattgperry@gmail.com>
2018-05-13 16:04:01 +0100

    adding rollup

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,29 @@
 import css from './css';
 import buildStyles from './css/render';
+import svg from './svg';
 import createStyler from './styler';
 import { Styler, Props } from './styler/types';
-import svg from './svg';
 
 const cache = new WeakMap<Element, Styler>();
 
 const createDOMStyler = (node: Element, props: Props) => {
-  const styler = (node instanceof SVGElement) ? svg(node) : css(node as HTMLElement, props);
+  const styler =
+    node instanceof SVGElement ? svg(node) : css(node as HTMLElement, props);
   cache.set(node, styler);
   return styler;
 };
 
-const getStyler = (node: Element, props: Props) => cache.has(node) ? cache.get(node) : createDOMStyler(node, props);
+const getStyler = (node: Element, props: Props) =>
+  cache.has(node) ? cache.get(node) : createDOMStyler(node, props);
 
-export default function(nodeOrSelector: Element | string, props: Props): Styler {
-  const node: Element = (typeof nodeOrSelector === 'string')
-    ? document.querySelector(nodeOrSelector)
-    : nodeOrSelector;
+export default function(
+  nodeOrSelector: Element | string,
+  props: Props
+): Styler {
+  const node: Element =
+    typeof nodeOrSelector === 'string'
+      ? document.querySelector(nodeOrSelector)
+      : nodeOrSelector;
 
   return getStyler(node, props);
 }
